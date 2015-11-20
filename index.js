05a1f82873ced0c4e7974995572058c40b51fd53
jaywcjlove <398188662@qq.com>
2015-11-21 05:00:06 +0800

    优化生成的map文件内容

--- index.js
+++ index.js
@@ -3,19 +3,47 @@ var through = require('through2');
 var path = require('path')
 var fs = require('fs')
 var UglifyJS = require("uglify-js")
+var gutil = require('gulp-util');
+
+var PluginError = gutil.PluginError;
 
 var PLUGIN_NAME = 'gulp-sourcemap'
 
 module.exports = function(options) {
+        
+    options = options || {};
 
     function minify(file, encoding, callback) {
-        if(!options) options = {};
+        // ignore empty files
+        if (file.isNull()) {
+          cb();
+          return;
+        }
+        if (file.isStream()) {
+            this.emit('error', new PluginError(PLUGIN_NAME,  'Streaming not supported'));
+            cb();
+            return;
+        }
+
         if(!options.outSourceMap) options.outSourceMap = file.relative;
-        if(!options.write)options.write = file.base;
+        if(!options.write) options.write = file.base;
+
+        var appInfo = require(process.cwd().toString() + '/package');
+
+        if(appInfo&&appInfo.version){
+            options.version = appInfo.version
+        }
+        // console.log("appInfo:",appInfo.version)
+        // console.log("appInfo:",process.cwd().toString())
 
         var result = UglifyJS.minify([file.path],options);
-
-        write(options.write + options.outSourceMap, result.map)
+        var map = JSON.parse(result.map);
+        var arr = map.sources;
+        arr.forEach(function(item,idx){
+            arr.splice(idx,1, item.toString().replace(process.cwd().toString(),''))
+        });
+        map.sources = arr;
+        write(options.write + options.outSourceMap,JSON.stringify(map))
         
         this.push(file);
         callback(null,file);
